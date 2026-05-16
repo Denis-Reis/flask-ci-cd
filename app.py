b332876a3f8abcecbe33d90e5bcbacebd349888a
Denis-Reis <almeida2801@outlook.com>
2026-05-16 11:40:38 -0300

    fix: ajustes de estilo para passar no Flake8

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 import os
 
 app = Flask(__name__)
@@ -9,9 +9,11 @@ def home():
     app_name = os.getenv("APP_NAME", "Minha Aplicação Flask")
     return f"<h1>{app_name}! And Hello CI/CD</h1>"
 
+
 @app.route("/saudacao")
 def saudacao():
     return "Olá, Deninho!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0", port=5000)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,11 @@
-import pytest, sys, os
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import sys
+import os
 from app import app
+
+
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 
 def test_home_route():
     tester = app.test_client()
@@ -9,11 +13,12 @@ def test_home_route():
     assert response.status_code == 200
     assert "Minha Aplicação Flask! And Hello CI/CD" in response.text
 
-def test_404_route():
 
+def test_404_route():
     tester = app.test_client()
     response = tester.get("/rota-inexistente")
     assert response.status_code == 404
+
 
 def test_env_variable(monkeypatch):
     monkeypatch.setenv("APP_NAME", "Teste Flask")
@@ -21,11 +26,9 @@ def test_env_variable(monkeypatch):
     response = tester.get("/")
     assert "Teste Flask" in response.text
 
+
 def test_saudacao_route():
     tester = app.test_client()
     response = tester.get("/saudacao")
     assert response.status_code == 200
     assert "Olá, Deninho!" in response.text
-
-
-
